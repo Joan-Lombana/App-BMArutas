890ba7314fbcfd7ef8b6e2ba7ee76c7de4fc31f7
Brayan Lerma <brayan.david.hurtado@correounivalle.edu.co>
2026-03-25 17:51:08 -0500

    login e inicio appconductor

--- appConductor/src/app/app.routes.ts
+++ appConductor/src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // Inicio con Splash
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'splash',
+    pathMatch: 'full'
   },
+  {
+    path: 'splash',
+    loadComponent: () => import('./pages/splash/splash.page').then(m => m.SplashPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then(m => m.DashboardPage)
+  },
+  // Fallback
+  {
+    path: '**',
+    redirectTo: 'splash'
+  }
 ];
